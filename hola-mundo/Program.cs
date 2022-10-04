4c35375426a49ab8b49864bbf82358237db67eff
Alberto <albertomorenogonzalez03@gmail.com>
2022-10-04 13:49:07 +0200

    Agregar archivos de proyecto.

---
+++ hola-mundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hola_mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Mundo!");
+        }
+    }
+}
